93ad3a708e2320cdc0a276bddd0bf60209f5d975
minhphonghp2003 <minhphonghp2003@gmail.com>
2023-04-12 21:09:35 +0700

    fix

--- findface.py
+++ findface.py
@@ -6,47 +6,47 @@ import os
 target = input('Enter avatar url: ')
 distance = float(input(
     "Enter distance between 2 images (leave blank to set default 0.6): ") or "0.6")
-img_folder = input("Enter images folder url(end with '/'): ")
-outp_name = input("Enter output file name (include .ext): ")
+imgFolder = input("Enter images folder url(end with '/'): ")
+outputFileName = input("Enter output file name (include .ext): ")
 
 
-def read_image(folder):
+def readFiles(folder):
     fileList = []
     for file in os.listdir(folder):
         fileList.append(folder + file)
     return fileList
 
 
-def encodeImage(image):
+def encodeFaces(image):
     target = cv.imread(cv.samples.findFile(image))
     img_rgb = cv.cvtColor(target, cv.COLOR_BGR2RGB)
-    img_encoding = face_recognition.face_encodings(img_rgb)
-    return img_encoding
+    encodedFaces = face_recognition.face_encodings(img_rgb)
+    return encodedFaces
 
 
 def detectionProcessing(imgFolder, encodedTarget, outputData, distance):
-    imageList = read_image(imgFolder)
+    imageList = readFiles(imgFolder)
     for image in imageList:
-        encodedImages = encodeImage(image)
-        if len(encodedImages) == 0:
+        encodedFaces = encodeFaces(image)
+        if len(encodedFaces) == 0:
             continue
-        result = face_recognition.compare_faces(
-            encodedImages, encodedTarget, distance)
+        comparedFaces = face_recognition.compare_faces(
+            encodedFaces, encodedTarget, distance)
 
-        if True in result:
-            face_distances = face_recognition.face_distance(
-                encodedImages, encodedTarget)
+        if True in comparedFaces:
+            faceDistances = face_recognition.face_distance(
+                encodedFaces, encodedTarget)
             outputData["data"].append(image)
-            outputData["distance"].append(min(face_distances))
+            outputData["distance"].append(min(faceDistances))
 
 
-with open(outp_name, "w") as outputFile:
-    encodedTarget = encodeImage(target)[0]
+with open(outputFileName, "w") as outputFile:
+    encodedTarget = encodeFaces(target)[0]
     outputData = {
         "data": [],
         "distance": []
     }
-    detectionProcessing(img_folder, encodedTarget, outputData, distance)
+    detectionProcessing(imgFolder, encodedTarget, outputData, distance)
     json_object = json.dumps(outputData, indent=4)
     outputFile.write(json_object)
     print(outputData)
